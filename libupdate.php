086726fddc256de139fbbf87a87c3b40c9d59b7b
Marcus Klaas <mail@marcusklaas.nl>
2015-04-13 22:06:51 +0200

    replace tabs by 4 spaces in php file

--- libupdate.php
+++ libupdate.php
@@ -6,44 +6,44 @@ $pwlib = 'encrypted/passwords';
 $pwlibext = 'txt';
 
 if(false === $doubleHash) {
-	die('failed reading hash file');
+    die('failed reading hash file');
 }
 
 function getBackupPath($libraryPath, $fileExtension) {
-	$i = 1;
+    $i = 1;
 
-	for(; file_exists($libraryPath.$i.'.'.$fileExtension); $i++);
+    for(; file_exists($libraryPath.$i.'.'.$fileExtension); $i++);
 
-	return $libraryPath.$i.'.'.$fileExtension;
+    return $libraryPath.$i.'.'.$fileExtension;
 }
 
 // TODO: indicate success/ failure using http status codes instead of response text!
 
 if(isset($_POST['pwhash']) && isset($_POST['newlib'])) {
-	if(sha1($_POST['pwhash']) !== $doubleHash) {
-		die('incorrect password');
+    if(sha1($_POST['pwhash']) !== $doubleHash) {
+        die('incorrect password');
     }
 
-	$libraryPath = $pwlib.'.'.$pwlibext;
+    $libraryPath = $pwlib.'.'.$pwlibext;
     $backupPath = getBackupPath($pwlib, $pwlibext);
 
-	if(file_exists($libraryPath) && ! rename($libraryPath, $backupPath)) {
-		die('rename failed');
-	}
-
-	if( ! file_put_contents($libraryPath, $_POST['newlib'])) {
-		die('file write failed');
+    if(file_exists($libraryPath) && ! rename($libraryPath, $backupPath)) {
+        die('rename failed');
     }
 
-	if(isset($_POST['newhash']) && ! file_put_contents($hashfile, sha1($_POST['newhash']))) {
-		if ( ! rename($backupPath, $libraryPath)) {
-			die('changed password, but was unable to restore library! This is a big deal!');
-		}
+    if( ! file_put_contents($libraryPath, $_POST['newlib'])) {
+        die('file write failed');
+    }
 
-		die('failed writing new hash to file');
-	}
+    if(isset($_POST['newhash']) && ! file_put_contents($hashfile, sha1($_POST['newhash']))) {
+        if ( ! rename($backupPath, $libraryPath)) {
+            die('changed password, but was unable to restore library! This is a big deal!');
+        }
 
-	die('success');
+        die('failed writing new hash to file');
+    }
+
+    die('success');
 }
 
 die('this is for machines only!');
